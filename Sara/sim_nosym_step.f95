85927f48c91271daae5b89efbfd18b4f02e7bf8c
Sara Vossoughi <sara.vossoughi@gmail.com>
2015-06-02 22:32:42 +0200

    Long standing pending changes.

--- Sara/sim_nosym_step.f95
+++ Sara/sim_nosym_step.f95
@@ -63,12 +63,15 @@ program ex1
     do in_i=0, (N_r*N_theta)-1 
       r_p = r_prime(in_i/N_theta)                                   ! current r-prime
       theta_p = theta_prime(MODULO(in_i, N_theta))                  ! current theta-prime
-      cosvar = cosCache(IAND(in_i-out_i, N_theta-1))  ! no significant change
-      sinvar = sinCache(IAND(in_i-out_i, N_theta-1))  ! possibility for optimization?
+      cosvar = cosCache(MODULO(in_i-out_i, N_theta))  ! no significant change
+      sinvar = sinCache(MODULO(in_i-out_i, N_theta))  ! possibility for optimization?
+      ! get rid of sinCache everywhere.
+      ! sinCache(x) = cosCache(x +- N_t/4), I think
 
       force_denominator = (r2+r_p*r_p-(2*r*r_p*cosvar))
       force_denominator = sqrt(force_denominator*force_denominator*force_denominator)
       force_mag_temp = (G*sigma(in_i)/(force_denominator)) * d_rd_theta
+      ! multiply force_mag_temp with r_prime, as this is probably a missing term here.
       force_r(out_i) = (r - r_p*cosvar) * force_mag_temp + force_r(out_i)
       force_theta(out_i) = (r_p*sinvar) * force_mag_temp + force_theta(out_i)
     end do
